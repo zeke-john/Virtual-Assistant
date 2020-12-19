619aa8749a15e7d4ff04112c4a65b2a5f11641c0
Zeke John <zekejohn118@gmail.com>
2020-12-18 20:34:06 -0800

    got multipe requests to work

--- main.py
+++ main.py
@@ -32,23 +32,25 @@ def button_go():
             query = word
             t2 = threading.Thread(target=Loading)
             t2.start() 
-            t2.join() 
+            t2.join()
             for url in search(query, tld="co.in", num=1, stop = 1, pause = 2):
                 webbrowser.open("https://google.com/search?q=%s" % query)
                 e = Entry(root, width=39, borderwidth=7)
-                e.grid(row=0, column=0, columnspan=3, padx=1, pady=1)
+                e.grid(row=0, column=0, columnspan=3, padx=1, pady=1)  
                 e.delete(0, END)
-
+                btn_go = Button(root, text="Voice Search",padx= 119, pady=15, command=threading.Thread(target=button_go).start, activebackground='#ff3333', bg='#5bc75d')  
+                btn_go.grid(row=1, columnspan=3)
 
         except:
             e = Entry(root, width=39, borderwidth=7)
             e.grid(row=0, column=0, columnspan=3, padx=1, pady=1)
             e.delete(0,END)
             e.insert(0, "Didn't get that, Try again")
+            btn_go = Button(root, text="Voice Search",padx= 119, pady=15, command=threading.Thread(target=button_go).start, activebackground='#ff3333', bg='#5bc75d')  
+            btn_go.grid(row=1, columnspan=3)
+        
 
-
-btn_go = Button(root, text="Voice Search",padx= 119, pady=15, command=threading.Thread(target=button_go).start, activebackground='#ff3333', bg='#5bc75d')  
+btn_go = Button(root, text="Voice Search",padx= 119, pady=15, command=threading.Thread(target=button_go).start, activebackground='#ff3333', bg='#5bc75d') 
 btn_go.grid(row=1, columnspan=3)
-
 root.resizable(False, False)
 root.mainloop()
