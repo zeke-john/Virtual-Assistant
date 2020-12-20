96e851aa9c8ff31a4a36a2e71e9ed64211a79949
Zeke John <zekejohn118@gmail.com>
2020-12-19 18:08:43 -0800

    done

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def Listening():
 def Loading():
     e = Entry(root, width=39, borderwidth=7)
     e.grid(row=0, column=0, columnspan=3, padx=1, pady=1)
-    e.insert(0, "Loading...")
+    e.insert(0, "Loading the top results on the web...")
 
 def button_go():
     try:
@@ -33,14 +33,12 @@ def button_go():
             r.adjust_for_ambient_noise(source)
             audio = r.listen(source)
             t1.join()
-
             try:
                 word = r.recognize_google(audio)
                 query = word
                 t2 = threading.Thread(target=Loading)
                 t2.start() 
                 t2.join()
-
                 for url in search(query, tld="co.in", num=1, stop = 1, pause = 2):
                     webbrowser.open("https://google.com/search?q=%s" % query)
                     e = Entry(root, width=39, borderwidth=7)
@@ -50,7 +48,6 @@ def button_go():
                     btn_go.grid(row=1, columnspan=3)
 
             except:
-                
                 e = Entry(root, width=39, borderwidth=7)
                 e.grid(row=0, column=0, columnspan=3, padx=1, pady=1)
                 e.delete(0,END)
@@ -66,7 +63,3 @@ btn_go = Button(root, image = mic_off ,text = '.', padx= 150, pady=7, command=th
 btn_go.grid(row=1, columnspan=3)
 root.resizable(False, False)
 root.mainloop()
-
-
-
-
